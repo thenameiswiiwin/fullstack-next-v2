1dca976e60f99a4f42be8f2f4973b5aefb98cb94
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-12-28 17:13:38 -0600

    logo

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import Card from './Card';
 import Image from 'next/image';
-import logo from '@/assets/images/logo.png';
+import logo from '@/assets/images/logo.svg';
 import SidebarLink from './SidebarLink';
 
 const links = [
